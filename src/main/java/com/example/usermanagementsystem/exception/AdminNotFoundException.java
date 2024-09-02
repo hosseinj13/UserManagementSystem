c66bd1995e0e4c3f6dc476b11d3014ad00410bd2
hossein javadi <hosseinj13@yahoo.com>
2024-09-02 18:28:48 +0330

    Add AdminNotFoundException.java for admin-related errors

---
+++ src/main/java/com/example/usermanagementsystem/exception/AdminNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.usermanagementsystem.exception;
+
+public class AdminNotFoundException extends RuntimeException {
+
+    public AdminNotFoundException(String message) {
+        super(message);
+    }
+}
