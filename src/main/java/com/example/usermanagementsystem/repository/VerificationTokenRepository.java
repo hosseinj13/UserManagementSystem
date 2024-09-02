9ce9e9c734f70cd6a4592bda10930bd7dfb1a0a3
hossein javadi <hosseinj13@yahoo.com>
2024-09-02 18:25:10 +0330

    Create VerificationTokenRepository.java for token management.

---
+++ src/main/java/com/example/usermanagementsystem/repository/VerificationTokenRepository.java
@@ -0,0 +1,17 @@
+package com.example.usermanagementsystem.repository;
+
+import com.example.usermanagementsystem.model.User;
+import com.example.usermanagementsystem.model.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+    Optional<VerificationToken> findByToken(String token);
+
+    Optional<VerificationToken> findByUser(User user);
+
+
+}
