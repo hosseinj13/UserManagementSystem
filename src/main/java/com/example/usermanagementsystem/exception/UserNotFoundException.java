ecfd5daa92e495f885bc47e6c6a95a0265939928
hossein javadi <hosseinj13@yahoo.com>
2024-09-02 18:32:05 +0330

    Add UserNotFoundException.java for user-related errors

---
+++ src/main/java/com/example/usermanagementsystem/exception/UserNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.usermanagementsystem.exception;
+
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
