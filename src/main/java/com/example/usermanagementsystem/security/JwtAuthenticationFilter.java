d08bae2af15e61667ccac6d1b66647fbcac6fadb
hossein javadi <hosseinj13@yahoo.com>
2024-09-02 18:39:10 +0330

    Implement JwtAuthenticationFilter.java for JWT authentication.

---
+++ src/main/java/com/example/usermanagementsystem/security/JwtAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.example.usermanagementsystem.security;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = jwtTokenProvider.resolveToken(request);
+
+        if (token != null && jwtTokenProvider.validateToken(token)) {
+            // Logging the JWT token
+            logger.info("JWT Token: {}", token);
+
+            String username = jwtTokenProvider.getUsernameFromToken(token);
+            String roles = jwtTokenProvider.getRolesFromToken(token).toString();
+
+            // Logging the extracted username and roles
+            logger.info("Extracted username: {}", username);
+            logger.info("Roles: {}", roles);
+
+            // Making an authentication object using roles
+            List<GrantedAuthority> authorities = Arrays.stream(roles.split(","))
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(username, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
